a78d8f9c064735739b753a7b329170ef7b8d58ae
Evan Miller <evanmiller2112@gmail.com>
2025-09-06 16:45:41 -0400

    removed target dir

--- target/debug/deps/spacetraders_oc_qwen3-f8bc8100655a1374.d
+++
@@ -1,5 +0,0 @@
-/Users/evanmiller/git/spacetraders_oc_qwen3/target/debug/deps/spacetraders_oc_qwen3-f8bc8100655a1374.d: src/main.rs
-
-/Users/evanmiller/git/spacetraders_oc_qwen3/target/debug/deps/libspacetraders_oc_qwen3-f8bc8100655a1374.rmeta: src/main.rs
-
-src/main.rs:
--- target/debug/deps/spacetraders_oc_qwen3-fbcd04857b68bc25.d
+++
@@ -1,5 +0,0 @@
-/Users/evanmiller/git/spacetraders_oc_qwen3/target/debug/deps/spacetraders_oc_qwen3-fbcd04857b68bc25.d: src/main.rs
-
-/Users/evanmiller/git/spacetraders_oc_qwen3/target/debug/deps/libspacetraders_oc_qwen3-fbcd04857b68bc25.rmeta: src/main.rs
-
-src/main.rs:
